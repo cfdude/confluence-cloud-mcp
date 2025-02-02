1cff8d5b3fb9dd0d96633e81046fecbf477e2df8
Aaron Bockelie <aaronsb@gmail.com>
2025-02-02 12:08:23 -0600

    chore: remove build files from git tracking
    - Remove build/ directory from git tracking
    - Files will still exist locally but won't be committed
    - Build files can be regenerated with npm run build

--- build/types/index.js
+++
@@ -1,8 +0,0 @@
-export class ConfluenceError extends Error {
-    code;
-    constructor(message, code) {
-        super(message);
-        this.code = code;
-        this.name = 'ConfluenceError';
-    }
-}
